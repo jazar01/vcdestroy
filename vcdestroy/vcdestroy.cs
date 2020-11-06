618f3d22c6395ccda3a290435812315ca47625f6
jazar01 <jim.azar@orasi.com>
2020-11-05 20:33:30 -0500

    Add project files.

---
+++ vcdestroy/vcdestroy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Data;
+
+namespace vcdestroy
+{
+    /// <summary>
+    /// Entrypoint for utility
+    /// Get parameters and then call the vcdestroyprocessor
+    /// </summary>
+    class vcdestroy
+    {
+        static int Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option<int>(
+                    aliases: new string[]{"--iterations", "-i" },
+                    getDefaultValue: () => 100000,
+                    description: "Number of random bytes to place"),
+
+                new Option<bool>(
+                    aliases: new string[]{"--generate", "-g" },
+                    description: "Generate a test file"),
+
+                new Option<bool>(
+                    aliases: new string[]{"--nodelete", "-n" },
+                    description: "Don't delete the file."),
+
+                new Option<int>(
+                    aliases: new string[]{"--size", "-s" },
+                    getDefaultValue: () => 1,
+                    description: "Size of test file in MB"),
+
+                new Argument<string>("FileSpec",
+                    description: "File search pattern. ex. test*.zip" )
+            };
+
+            rootCommand.Description = "VC destroyer";
+
+            rootCommand.Handler = CommandHandler.Create<int, bool, bool, int, string>
+                ((iterations, generate, nodelete, size, fileSpec) =>
+                {
+
+                    Console.WriteLine($"File Spec = {fileSpec}");
+                    if (!generate) Console.WriteLine($"Iterations = {iterations}");                
+                    if (generate) Console.WriteLine($"gen = {generate}");
+                    if (!generate) Console.WriteLine($"Don't delete = {nodelete}");
+                    if (generate) Console.WriteLine($"size = {size}");
+
+                    if (generate)
+                    {
+                        FileStream fs = new FileStream(fileSpec, FileMode.Create);
+
+                        while (size-- > 0)
+                        {
+                            byte[] buffer = new byte[1024 * 1024];
+                            Array.Clear(buffer, 0, buffer.Length);
+                            fs.Write(buffer, 0, buffer.Length);
+                        }
+                        fs.Dispose();
+                    }
+                    else
+                    { 
+
+                    vcdestroyProcessor pp = new vcdestroyProcessor(iterations, fileSpec, nodelete);
+                    }
+                   
+                });
+
+            return rootCommand.InvokeAsync(args).Result;
+
+        }
+    }
+}
---
+++ vcdestroy/vcdestroyProcessor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+
+namespace vcdestroy
+{
+    public class vcdestroyProcessor
+    {
+        /// <summary>
+        /// This is the main method for purging files
+        /// </summary>
+        /// <param name="iterations"></param>
+        /// <param name="fileSpec"></param>
+        public vcdestroyProcessor(int iterations,  string fileSpec, bool nodelete)
+        {
+            DateTime startTime = DateTime.Now;
+
+            wipe(fileSpec, iterations, nodelete);
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("{0} Destroyed in {1:f1} Seconds", fileSpec,  elapsedTime.TotalSeconds);
+        }
+
+        private void wipe(string filespec, int iterations, bool nodelete)
+        {
+            const int BUFFER_SIZE = 65536;
+
+            FileInfo File = new FileInfo(filespec);
+            if (!File.Exists)
+                throw new FileNotFoundException();
+
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+                Random rnd = new Random();
+                FileStream fs = File.OpenWrite();
+            Console.WriteLine("Wiping headers");
+            // overwrite headers 99 times
+            for (int i = 0; i < 99; i++)
+            {
+
+                // write 65K random bytes at the beginning of the file
+                //   this wipes out the volume header and master keys
+                fs.Seek(0, SeekOrigin.Begin);
+                rnd.NextBytes(buffer);
+                int BytesToWrite = (int)Math.Min(BUFFER_SIZE, File.Length);
+                fs.Write(buffer, 0, BytesToWrite);
+
+                // if the file size is more than 65K the write random bytes at the end of the file
+                //   this wipes out the backup volume header for any hidden volume
+                long endbytes = File.Length - BUFFER_SIZE;
+                if (endbytes > 0)
+                {
+                    fs.Seek(-BUFFER_SIZE, SeekOrigin.End);
+                    rnd.NextBytes(buffer);
+                    fs.Write(buffer, 0, BUFFER_SIZE);
+                }
+
+                // write to second set of 65K
+                //     this wipes out the backup header for the hidden volume
+
+                if (File.Length > BUFFER_SIZE)
+                {
+                    fs.Seek(BUFFER_SIZE, SeekOrigin.Begin);
+                    rnd.NextBytes(buffer);
+                    BytesToWrite = (int)Math.Min(BUFFER_SIZE, File.Length - BUFFER_SIZE);
+                    fs.Write(buffer, 0, BytesToWrite);
+                }
+
+
+                // write to second from end set of 65K
+                //      this wipes out the backup header for the main volume
+                if (File.Length > BUFFER_SIZE * 2)
+                {
+                    fs.Seek(-BUFFER_SIZE * 2, SeekOrigin.End);
+                    rnd.NextBytes(buffer);
+                    fs.Write(buffer, 0, BUFFER_SIZE);
+                }
+
+                fs.Flush();
+            }
+
+            // write random bytes to random locations throughout the rest of the file
+            if (File.Length > 2* BUFFER_SIZE)
+            {
+                Console.WriteLine("Writing random bytes in random locations");
+                while (iterations-- > 0)
+                {
+                    long location = (long) (rnd.NextDouble() * (File.Length - 2*BUFFER_SIZE)) + BUFFER_SIZE;
+                    fs.Seek(location, SeekOrigin.Begin);
+                    int bytes = rnd.Next(1, BUFFER_SIZE);
+                    rnd.NextBytes(buffer);
+                    fs.Write(buffer, 0, bytes);
+                }
+            }
+
+               fs.Dispose();
+
+            if (!nodelete)
+               File.Delete();
+        }
+    }
+}
